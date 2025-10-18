6398c2358f3ec5934f62b6bed79e36c45ef0109a
Sam Bonnekamp <sam@bonnekamp.net>
2025-10-19 01:10:52 +0800

    requests now more efficient

--- screen_date/screen_date.ino
+++ screen_date/screen_date.ino
@@ -5,8 +5,9 @@
 WiFiClient client; //yucky OOP
 int port = 8080;
 
+int data_requests = 0;
 char buff[64];
-char qqq[30];
+char gold[30];
 char *dow[] = {"Sunday",
 	      "Monday",
 	      "Tuesday",
@@ -18,19 +19,25 @@ char *dow[] = {"Sunday",
 
 
 void make_req(char* endpoint){
+  int pre_connection = 0;
+  if(WiFi.status() != WL_CONNECTED){
+    pre_connection = 1;
+    screen_put_string("Connecting");
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+    while (WiFi.status() != WL_CONNECTED){
+      delay(700);
+    }
+    clear_display();
+  }
   IPAddress base(192,168,50,196);
   int res; //connection result
   res = client.connect(base, port);
-  clear_display();
   if(res){
     screen_put_string("success!");
     client.println(endpoint);
-    clear_display();
     while(!client.available()){ //check if ready to recieve bytes TODO: create an auto stop
       delay(20);
     }
-    sprintf(buff,"%d bytes to read", client.available());
-    screen_put_string(buff);
     int index = 0;
     char construction[13];
     while(client.available()){
@@ -42,12 +49,16 @@ void make_req(char* endpoint){
     if(strcmp(endpoint, "HIII")==0){
       setTime(strtoul(construction, NULL, 10));
     }else{
-      strcpy(qqq, construction);
+      strcpy(gold, construction);
     }
     clear_display();
   }else{
     screen_put_string("failed!");
+    delay(2000);
+    clear_display();
   }
+  if(pre_connection == 1) //only disconnect wifi if this function connected it
+    WiFi.disconnect();
 }
 
 
@@ -67,9 +78,10 @@ void setup() {
   clear_display();
   screen_put_string("Connected!");
   delay(500);
+  clear_display();
   make_req("HIII");
   delay(500);
-  make_req("qqq");
+  make_req("/gold");
   delay(500);
   WiFi.disconnect();
 }
@@ -83,10 +95,16 @@ void loop() {
   sprintf(buff, "%02d.%02d.%02d", day(t), month(t), year(t));
   screen_put_string(buff);
 
-  delay(10000);
-
+  delay(7000);
+  if(data_requests > 90){
+    make_req("/gold");
+    data_requests = 0;
+  }
+  data_requests++;
   clear_display();
-  sprintf(buff, "QQQ:  %s", qqq);
+  screen_put_string("GOLD:");
+  set_cursor(0, 1);
+  sprintf(buff, "$%s", gold);
   screen_put_string(buff);
-  delay(10000);
+  delay(7000);
 }
